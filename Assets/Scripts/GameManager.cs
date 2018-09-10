66c790e23cd6bca54758d4cbdbaf7e76357bc8c8
anca-rosu <anca.rosu95@gmail.com>
2018-09-10 23:12:01 +0300

    Initial commit: YarnSpinner, .gitignore, GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Load the first scene
+        LoadScene(0);
+    }
+
+    //LoadScene loads a scene with a given name
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    //LoadScene loads a scene with a given scene index (>0)
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
